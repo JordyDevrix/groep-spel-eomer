2d67af364e6636fbb14c27d9f99a6178eaf5e2f2
Dennis Heemskerk <s1148788@student.hsleiden.nl>
2023-10-10 21:15:29 +0200

    Hoofdmenu.py versie 2.0 (kleine verbeteringen)

--- hoofdmenu.py
+++ hoofdmenu.py
@@ -4,8 +4,8 @@ def hoofdmenu():
     root = Tk()
     root.geometry("1000x562")
     root.title("Hoofdmenu")
-    root.configure(highlightbackground='black')
 
+#---------------------ACHTERGROND/TEKST--------------------------
     bg = PhotoImage(file="images/LOTRBG.png")
     canvasBG = Canvas(root, width=1000,
                       height=562)
@@ -14,7 +14,7 @@ def hoofdmenu():
                           anchor = "nw")
     canvasBG.create_text(500, 50, text= "Welkom bij Lord of The Rings Legends", font=("Arial", 28))
 
-
+# ------------------------BUTTONS--------------------------------
     speel_button = Button(root, text="Speel",
                     font=("Arial", 24),
                     command=lambda : scherm_speel(root))
@@ -22,7 +22,7 @@ def hoofdmenu():
 
     kies_karakter_button = Button(root, text="Kies karakter",
                                   font=("Arial", 24),
-                                  command= lambda : scherm_speel(root))
+                                  command= lambda : scherm_kies_karakter(root))
 
     afsluiten_button = Button(root, text="Afsluiten",
                                   font=("Arial", 24),
@@ -40,24 +40,25 @@ def hoofdmenu():
                                        anchor = "nw",
                                        window = afsluiten_button)
 
+# ---------------------------------------------------------------
     root.mainloop()
 
-
+# --==-- --==-- --==-- --==-- --==-- --==-- --==-- --==-- --==--
 def scherm_speel(root):
     for widget in root.winfo_children():
         widget.destroy()
 
-    label = Label(root, text="Hallo")
+    label = Label(root, text="Speel pagina")
     label.grid(column = 0, row = 0)
 
     labl = Label(root, text="Hallo")
     labl.grid(column=1, row=1)
 
+# --==-- --==-- --==-- --==-- --==-- --==-- --==-- --==-- --==--
 def scherm_kies_karakter(root):
     for widget in root.winfo_children():
         widget.destroy()
-
-        label = Label(root, text="Hallo")
+        label = Label(root, text="Kies karakter pagina")
         label.grid(column=0, row=0)
 
         labl = Label(root, text="Hallo")
