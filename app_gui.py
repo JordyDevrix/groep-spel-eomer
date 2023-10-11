6ffc42fe101d81126c38abf5c69dd925065ad54f
Sem Star <s1150030@student.hsleiden.nl>
2023-10-11 17:28:27 +0200

    locatie_tijd toevoegven in app_gui

--- app_gui.py
+++ app_gui.py
@@ -44,8 +44,29 @@ def settings(root):
     cancel_button = Button(top_bar, width=3, height=1, text="X", command=lambda: menu(root))
     cancel_button.pack(padx=5, pady=5, side="right")
 
+def tijd_locatie_weergeven(root):
+    for widget in root.winfo_children():
+        widget.destroy()
 
+    tijd = "10:12"
+    locatie = "Rivendel"
+
+    frame1 = Frame(root, width=300, height=120, highlightbackground="blue", highlightthickness=6)
+    frame1.place(x=600, y=340)
+
+    lt_label = Label(frame1, text=f"{tijd} en {locatie}", font=("Roboto", 24))
+    lt_label.grid(row=0, column=0, pady=20)  # Center label vertically
+
+    close_button = Button(frame1, text="x", command=lambda: spel_spelen(root))
+    close_button.grid(row=1, column=0)
 def spel_spelen(root):
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    button_tl = Button(root, text="Tijd/Locatie", command=lambda: tijd_locatie_weergeven(root))
+    button_tl.place(x=20, y=750)
+
+    root.mainloop()
     for widget in root.winfo_children():
         widget.destroy()
 
--- locatie_tijd.py
+++ locatie_tijd.py
@@ -4,26 +4,26 @@ def main(root):
     for widget in root.winfo_children():
         widget.destroy()
 
-    button_tl = Button(root, text= "Tijd/Locatie", command= lambda: tijd_locatie_weergeven(root))
+    button_tl = Button(root, text="Tijd/Locatie", command=lambda: tijd_locatie_weergeven(root))
     button_tl.place(x=20, y=750)
 
     root.mainloop()
-
 
 def tijd_locatie_weergeven(root):
     for widget in root.winfo_children():
         widget.destroy()
 
-    tijd= "10:12"
-    locatie= "Rivendel"
+    tijd = "10:12"
+    locatie = "Rivendel"
 
-    lt_label = Label(root, text=f"{tijd} en {locatie}", font="Roboto, 24")
-    lt_label.place(x=520, y=350)
-    close_button = Button(root, text= "x", command=lambda: main(root))
-    close_button.place(x=700, y=420)
+    frame1 = Frame(root, width=300, height=120, highlightbackground="blue", highlightthickness=6)
+    frame1.place(x=600, y=340)
 
+    lt_label = Label(frame1, text=f"{tijd} en {locatie}", font=("Roboto", 24))
+    lt_label.grid(row=0, column=0, pady=20)  # Center label vertically
 
-
+    close_button = Button(frame1, text="x", command=lambda: main(root))
+    close_button.grid(row=1, column=0)
 
 if __name__ == '__main__':
     root = Tk()
