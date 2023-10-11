fb853e0b892e59803ec5e897e0d498af4e5b8671
Sem Star <s1150030@student.hsleiden.nl>
2023-10-11 13:03:11 +0200

    character creation mooier gemaakt

--- character_maken.py
+++ character_maken.py
@@ -29,13 +29,18 @@ def character_ophalen(name):
 
 
 
-def character_maken(venster):
+def character_maken(venster, menu):
     for widget in venster.winfo_children():
         widget.destroy()
 
+    top_bar = Frame(venster, bg="grey", height=40)
+    top_bar.pack(fill="both")
 
-    naam_input_frame = Frame(venster)
-    naam_input_frame.place(anchor= 'center', relx= 0.5, rely= 0.5)
+    cancel_button = Button(top_bar, width=3, height=1, text="X", command=lambda: menu(venster))
+    cancel_button.pack(padx=5, pady=5, side="right")
+
+    naam_input_frame = Frame(venster, borderwidth=5, relief="solid", padx=15, pady=15)
+    naam_input_frame.place(anchor='center', relx=0.5, rely=0.5)
 
     label = Label(naam_input_frame, text="Vul uw naam in.", font="Roboto, 24")
     label.pack()
@@ -43,9 +48,9 @@ def character_maken(venster):
     name_input = Entry(naam_input_frame, width=10, font="Roboto, 24")
     name_input.pack()
 
-    haal_naam_op_knop = Button(venster, text="Haal tekst op", command= lambda : naam_ophalen(venster, name_input))
+    haal_naam_op_knop = Button(venster, text="Haal tekst op", command=lambda: naam_ophalen(venster, name_input))
     haal_naam_op_knop.pack()
-
+    return venster
 
 
 
@@ -55,9 +60,9 @@ def naam_ophalen(venster, name_input):
     name_input.delete(0, tkinter.END)
     name = name_inhoud
     label_huidge_naam = Label(venster, text= f"Uw huidige naam: {name}")
-    label_huidge_naam.place(anchor= 'center', relx= 0.5, rely=0.6)
+    label_huidge_naam.place(anchor= 'center', relx= 0.5, rely=0.65)
     doorgaan_button = Button(venster, text="Ga door naar volgende keuze.", font=("Arial, 10"), command=lambda : ras_kiezen(venster))
-    doorgaan_button.place(anchor= 'center', relx= 0.5, rely= 0.56)
+    doorgaan_button.place(anchor= 'center', relx= 0.5, rely= 0.6)
 
 def ras_kiezen(venster):
     for widget in venster.winfo_children():
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from hoofdmenu import hoofdmenu
+from character_maken import character_maken
 def main():
     hoofdmenu()
+
 
 
 if __name__ == '__main__':
