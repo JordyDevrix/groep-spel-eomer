812d5e1ad19cc3b06be0ad9986616f8179597e1e
Eunice  Adegoju <s1149010@student.hsleiden.nl>
2023-10-12 09:23:40 +0200

    Ras functies toegevoegd

--- meerdere_keuzes.py
+++ meerdere_keuzes.py
@@ -1,6 +1,5 @@
 import json
 import tkinter as tk
-from tkinter import *
 
 
 class TekstAvontuur:
@@ -14,7 +13,7 @@ class TekstAvontuur:
         top_bar = tk.Frame(root, bg="grey", height=40)
         top_bar.pack(fill="both")
 
-        self.beschrijving_label = tk.Label(root, text="", wraplength=800, padx=10, pady=10)
+        self.beschrijving_label = tk.Label(root, justify="left", text="", wraplength=800, padx=10, pady=10)
         self.beschrijving_label.place(anchor="center", relx=0.5, rely=0.9)
 
         self.button_frame = tk.Frame(root)
@@ -27,6 +26,9 @@ class TekstAvontuur:
         root.mainloop()
 
     def update_interface(self):
+        het_ras_van_de_speler_is_mens = False
+        het_ras_van_de_speler_is_dwerg = True
+
         locatie_data = self.gegevens["locaties"][self.huidige_locatie]
         self.beschrijving_label.config(text=locatie_data["beschrijving"])
 
@@ -38,10 +40,24 @@ class TekstAvontuur:
         for i, keuze in enumerate(keuzes):
             keuze_text = list(keuze.keys())[0]
             nieuwe_locatie = list(keuze.values())[0]
-            button = tk.Button(self.button_frame, width=47, height=30, bg="grey", text=keuze_text,
-                               command=lambda loc=nieuwe_locatie: self.kies_optie(loc))
-            button.grid(padx=20, pady=5, column=i, row=0)
-            self.keuze_buttons.append(button)
+
+            if "Dwerg" in keuze_text:
+                if het_ras_van_de_speler_is_dwerg:
+                    button = tk.Button(self.button_frame, width=47, height=30, bg="grey", text=keuze_text,
+                                       command=lambda loc=nieuwe_locatie: self.kies_optie(loc))
+                    button.grid(padx=20, pady=5, column=i, row=0)
+                    self.keuze_buttons.append(button)
+            elif "Mens" in keuze_text:
+                if het_ras_van_de_speler_is_mens:
+                    button = tk.Button(self.button_frame, width=47, height=30, bg="grey", text=keuze_text,
+                                       command=lambda loc=nieuwe_locatie: self.kies_optie(loc))
+                    button.grid(padx=20, pady=5, column=i, row=0)
+                    self.keuze_buttons.append(button)
+            else:
+                button = tk.Button(self.button_frame, width=47, height=30, bg="grey", text=keuze_text,
+                                   command=lambda loc=nieuwe_locatie: self.kies_optie(loc))
+                button.grid(padx=20, pady=5, column=i, row=0)
+                self.keuze_buttons.append(button)
 
     def kies_optie(self, nieuwe_locatie):
         self.huidige_locatie = nieuwe_locatie
@@ -62,7 +78,6 @@ def start_tekst_avontuur(root):
     for widget in root.winfo_children():
         widget.destroy()
     TekstAvontuur(root, "files/avontuurgegevens.json")
-
 
 
 if __name__ == "__main__":
