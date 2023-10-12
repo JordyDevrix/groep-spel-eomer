9dbcf68448f70cd1396e5822a211f24b6245b2f1
Bjornraaf <30087@ma-web.nl>
2023-10-12 10:52:11 +0200

    meerdere_keuzes.py werkt if main weggehaald en akn alleen starten in

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from tkinter import *
-from hoofdmenu import hoofdmenu
-from character_maken import character_maken
 from app_gui import applicatie_gui
+
+
 def main():
     applicatie_gui()
-
 
 
 if __name__ == '__main__':
