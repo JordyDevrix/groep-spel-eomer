48756d36d7ba76deb1e27d81368197e44b029404
JordyDevrix <2109jordy2241@gmail.com>
2023-10-12 11:08:22 +0200

    main.py toegevoegd aan commit

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from tkinter import *
 from hoofdmenu import hoofdmenu
 from character_maken import character_maken
 from app_gui import applicatie_gui
-
-
 def main():
     applicatie_gui()
+
 
 
 if __name__ == '__main__':
