2ed8741d9d9aa28b54ed3a5457e6be1714a6f21e
JordyDevrix <2109jordy2241@gmail.com>
2023-10-12 13:10:30 +0200

    check for more clean code and move music call to applicatie_gui func

--- app_gui.py
+++ app_gui.py
@@ -44,36 +44,6 @@ def settings(root):
 #     close_button.grid(row=1, column=0)
 
 
-# def spel_spelen(root):
-#     for widget in root.winfo_children():
-#         widget.destroy()
-#
-#     button_tl = Button(root, text="Tijd/Locatie", command=lambda: tijd_locatie_weergeven(root))
-#     button_tl.place(x=20, y=750)
-#
-#     root.mainloop()
-#     for widget in root.winfo_children():
-#         widget.destroy()
-#
-#     top_bar = Frame(root, bg="grey", height=40)
-#     top_bar.pack(fill="both")
-#
-#     avonturen_frame = Frame(root)
-#     avonturen_frame.place(anchor="center", relx=0.5, rely=0.5)
-#
-#     avontuur_een = Frame(avonturen_frame, width=400, height=700, bg="grey")
-#     avontuur_een.grid(padx=20, pady=5, column=0, row=0)
-#
-#     avontuur_twee = Frame(avonturen_frame, width=400, height=700, bg="grey")
-#     avontuur_twee.grid(padx=20, pady=5, column=1, row=0)
-#
-#     avontuur_drie = Frame(avonturen_frame, width=400, height=700, bg="grey")
-#     avontuur_drie.grid(padx=20, pady=5, column=2, row=0)
-#
-#     cancel_button = Button(top_bar, width=3, height=1, text="X", command=lambda: menu(root))
-#     cancel_button.pack(padx=5, pady=5, side="right")
-
-
 def menu(root):
     for widget in root.winfo_children():
         widget.destroy()
@@ -83,30 +53,42 @@ def menu(root):
     bg_label = Label(root, image=bg_image)
     bg_label.place(relwidth=1, relheight=1)
 
-    threading.Thread(target=music).start()
-
     top_bar = Frame(root, bg="grey", height=40)
     top_bar.pack(fill="both")
 
     menu_button_frame = Frame(height=200, width=400, bg="Green")
     menu_button_frame.place(anchor="center", relx=0.5, rely=0.5)
 
-    maak_character = Button(menu_button_frame, text="CREATE A CHARACTER", font="Roboto, 20", width=40, command=lambda: character_maken(root, menu))
+    maak_character = Button(menu_button_frame,
+                            text="CREATE A CHARACTER",
+                            font="Roboto, 20",
+                            width=40,
+                            command=lambda: character_maken(root, menu))
     maak_character.pack(padx=10, pady=10, fill="both")
 
     # speel_spel = Button(menu_button_frame, text="PLAY GAME", font="Roboto, 20", width=40, command=lambda: spel_spelen(root))
     # speel_spel.pack(padx=10, pady=10, fill="both")
 
-    settings_optie = Button(menu_button_frame, text="SETTINGS", font="Roboto, 20", width=40, command=lambda: settings(root))
+    settings_optie = Button(menu_button_frame,
+                            text="SETTINGS",
+                            font="Roboto, 20",
+                            width=40,
+                            command=lambda: settings(root))
     settings_optie.pack(padx=10, pady=10, fill="both")
 
-    admin_opties = Button(menu_button_frame, text="ADMIN MODE", font="Roboto, 20", width=40, command=lambda: admin_inlogscherm(root, menu))
+    admin_opties = Button(menu_button_frame,
+                          text="ADMIN MODE",
+                          font="Roboto, 20",
+                          width=40,
+                          command=lambda: admin_inlogscherm(root, menu))
     admin_opties.pack(padx=10, pady=10, fill="both")
 
     root.mainloop()
 
 
 def applicatie_gui():
+    threading.Thread(target=music).start()
+
     root = Tk()
     root.geometry("1400x800")
     root.resizable(False, False)
