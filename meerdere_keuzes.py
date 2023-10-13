ad8e886d6d981a97d3e5bc87e7efbf1081424add
eoude <s1150629@student.hsleiden.nl>
2023-10-13 11:23:36 +0200

    actie/locatie button

--- meerdere_keuzes.py
+++ meerdere_keuzes.py
@@ -40,10 +40,25 @@ def update_interface(huidige_locatie, gegevens, beschrijving_label, button_frame
             button.grid(padx=20, pady=5, column=i, row=0)
 
 
+def tijd_locatie_weergeven(huidige_locatie):
+
+    locatie = huidige_locatie
+
+    frame1 = tk.Frame(width=300, height=120, highlightbackground="blue", highlightthickness=6)
+    frame1.place(x=600, y=340)
+
+    lt_label = tk.Label(frame1, text=f"{locatie}", font=("Roboto", 24))
+    lt_label.grid(row=0, column=0, pady=20)  # Center label vertically
+    close_button = tk.Button(frame1, text="x", command=lambda: (close_button.destroy(), lt_label.destroy(), frame1.destroy()))
+    close_button.grid(row=1, column=0)
+
+
 def kies_optie(nieuwe_locatie, huidige_locatie, gegevens, beschrijving_label, button_frame):
     locatie_data = gegevens["locaties"][huidige_locatie]
     keuzes = locatie_data["keuzes"]
     geselecteerde_keuze = next((keuze for keuze in keuzes if list(keuze.values())[0] == nieuwe_locatie), None)
+    button_tl = tk.Button(text="Actie/Locatie", command=lambda: tijd_locatie_weergeven(huidige_locatie))
+    button_tl.place(x=20, y=750)
     if geselecteerde_keuze:
         geselecteerde_keuze_tekst = list(geselecteerde_keuze.keys())[0]
         gemaakte_keuzes.append(geselecteerde_keuze_tekst)
@@ -59,6 +74,7 @@ def kies_optie(nieuwe_locatie, huidige_locatie, gegevens, beschrijving_label, bu
 
         for knop in button_frame.winfo_children():
             knop.destroy()
+    tk.mainloop()
 
 
 def start_tekst_avontuur(root, bestand, menu):
